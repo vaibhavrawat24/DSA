311e08564091fbcdb5a9f504c87125665dac4093
Vaibhav <rawatvaibhav42@gmail.com>
2022-10-26 17:03:50 +0530

    edited code

--- Dynamic Programming/04-frog-jump.cpp
+++ Dynamic Programming/04-frog-jump.cpp
@@ -49,3 +49,24 @@ int frogJump(int n, vector<int> &heights)
 }
 
 //Space Reduction
+
+#include <bits/stdc++.h> 
+
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> dp(n,0);
+    int prev=0;
+    int prev2=0;
+    for(int i=1;i<n;i++)
+    {
+        int fs=prev+abs(heights[i]-heights[i-1]);
+        int ss=INT_MAX;
+        if(i>1) ss=prev2+abs(heights[i]-heights[i-2]);
+        int curi=min(fs,ss);
+        prev2=prev;
+        prev=curi;
+    }
+    
+    return prev;
+    
+}
