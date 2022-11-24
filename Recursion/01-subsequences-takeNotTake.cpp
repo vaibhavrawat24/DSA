74481261a14e79c2f94ccf949264b44014083f41
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-24 18:57:26 +0530

    added takenottake code recursion

---
+++ Recursion/01-subsequences-takeNotTake.cpp
@@ -0,0 +1,35 @@
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+void pick_notpick(vector<int> ds,int arr[],int idx,int n)
+{
+    if(idx==n)
+    {
+        for(auto it:ds) cout<<it<<" ";
+        if(ds.size()==0) cout<<"{}";
+        cout<<endl;
+        return;
+    }
+
+    //not take condition, this element is not added to subsequence
+    pick_notpick(ds,arr,idx+1,n);
+
+    //take the particular index into the subsequence
+    ds.push_back(arr[idx]);
+    pick_notpick(ds,arr,idx+1,n);
+    ds.pop_back();
+
+}
+ 
+int main(){
+    int arr[]={3,1,2};
+    vector<int> ds;
+
+    pick_notpick(ds,arr,0,3);
+ 
+    return 0;
+}
